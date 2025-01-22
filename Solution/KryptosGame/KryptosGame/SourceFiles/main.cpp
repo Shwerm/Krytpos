d44c5c01b62438393f3349be81066a4b281c3ecb
Shwerm <95546677+Shwerm@users.noreply.github.com>
2025-01-22 15:23:06 +0000

    feat: SFML linking test (Success)
    - Added basic SFML window creation and game loop to main.cpp to test SFML linking which was successful

--- Solution/KryptosGame/KryptosGame/SourceFiles/main.cpp
+++ Solution/KryptosGame/KryptosGame/SourceFiles/main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML window");
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        while (const std::optional event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Update the window
+        window.display();
+    }
 }
